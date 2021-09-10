5e2b6fa73f38ae9d6b33de4de1a2d860278cfd44
saiprasad_devare <saiprasaddevare12@gmail.com>
2021-09-10 17:47:11 +0530

    Enhancement :: React-Tour : Add and use react library for the sample app tour

--- app/javascript/components/home.jsx
+++ app/javascript/components/home.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Joyride from 'react-joyride';
 
 const Home = () => {
+  const [steps, setSteps] = useState([
+    {
+      target: '#name',
+      content: 'This is name field',
+      disableBeacon: true
+    },
+    {
+      target: '#actual_name',
+      content: 'This is actual name',
+    }
+  ]);
+
+  const [display, setDisplay] = useState(false)
+
+  useEffect(() => {
+    setDisplay(true)
+  }, [])
+
   return (
-    <div>HOme page</div>
+    <div>
+      <Joyride steps={steps} showSkipButton/>
+      <div style={{ border: '1px solid black' }}>
+        <div id='name'> Name </div>
+        <div id='actual_name'> Saiprasad Devare </div>
+      </div>
+
+      <div style={{ border: '1px solid black', marginTop: '3rem' }}>
+        <div id='address'> Address </div>
+        <div id='actual_address'> Shri Dadasaheb chougule colony </div>
+      </div>
+    </div>
   )
 }
 
